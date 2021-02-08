c36af59d0791f7828853e05c0b46aa4f9042fc22
Dillon Harris <dillon.keith.harris96@gmail.com>
2021-02-08 10:35:54 -0700

    Initial set up
    Sets up the app and installs needed packages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueKonva from "vue-konva";
 
-Vue.config.productionTip = false
+Vue.use(VueKonva);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
